6bf5f4016eea9dca09a36378d4700cadc4cd5010
unknown <stephanrossi@gmail.com>
2022-03-09 21:43:20 -0300

    Funcionando

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import path from 'path'
 import { fileURLToPath } from 'url';
 import mustache from 'mustache-express'
+import bodyParser from 'body-parser'
 
 import axios from 'axios'
 
@@ -13,6 +14,9 @@ const app = express()
 
 app.use(cors())
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
 //Set view engine
 app.set("view engine", "mustache")
 app.set("views", path.join(__dirname, "views"))
@@ -21,12 +25,25 @@ app.engine("mustache", mustache())
 //Set public folder
 app.use(express.static(path.join(__dirname, "../public")))
 
+app.use(express.json())
+
 //Routes
-app.get('/api', async (req, res) => {
-  const response = await axios.get('https://system.solides.com/pt-BR/api/v1/profiler/19441', {
-    headers: { Authorization: 'Token token=9e7545e473473c3274173cc77e39f2fe995d703a93d6d23539b8' }
-  })
-  console.log(response.data);
+app.get('/', (req, res) => {
+  res.render("pages/index")
+})
+
+app.post('/api', async (req, res) => {
+  const cpf = req.body.cpf
+
+  try {
+    await axios.get(`https://system.solides.com/pt-BR/api/v1/colaboradores/existe/${cpf}`, {
+      headers: { Authorization: 'Token token=9e7545e473473c3274173cc77e39f2fe995d703a93d6d23539b8' }
+    })
+    res.send('Funcionário ativo.')
+  }
+  catch (e) {
+    res.send(e.response.data.messages);
+  }
 })
 
 //404
